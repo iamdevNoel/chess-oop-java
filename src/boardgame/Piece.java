c5acacd719c176983f587352142386bff2526ed4
Gabriel Noberto (iamdevnoel) <contato.gabrielnoberto@gmail.com>
2023-10-12 20:23:59 -0300

    Created Board and Piece classes at boardgame package

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		position = null; //Positions are defined only after game has started
+		this.board = board;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+	
+}
